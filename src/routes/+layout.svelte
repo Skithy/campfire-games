f503af363b425432c8a0f854f740988a9f5c3121
Tony Dong <tony@honeyinsurance.com>
2025-12-12 23:09:37 +1100

    removed initial pages

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,13 +2,10 @@
   import "../app.css"
   import "../lib/css/all.css"
 
-  import Header from "./Header.svelte"
-
   let { children } = $props()
 </script>
 
 <div class="flex min-h-screen flex-col">
-  <Header />
 
   <main class="flex flex-1 flex-col">
     {@render children()}
